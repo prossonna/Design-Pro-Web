27b12f748088a1cbfe7057e338050e82f808986a
Prossonna <chaudhary.prasanna88@gmail.com>
2021-05-29 14:32:58 +0545

    file managed in Gulp and added header

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 const gulp = require('gulp');
+const { src, watch, series, dest, parallel } = require('gulp');
+const clean = require('gulp-clean');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 
 
+
 // Functions
+
+// clean dist
+function clear() {
+    return gulp.src('dist/*', {
+        read: false
+    })
+        .pipe(clean())
+}
 
 // Compile scss into css file
 function compileScss() {
@@ -18,8 +31,43 @@ function copyHtml() {
         .pipe(gulp.dest('dist'))
 }
 
+// Minified image
+function imageMinify() {
+    return gulp.src('src/image/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/image'))
+}
+
+// Concating Js
+function concatJs(){
+    return gulp.src('src/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'))
+}
+
+// Watch Files
+function watchFiles() {
+    watch('src/scss/*', compileScss);
+    watch('src/*.html', copyHtml);
+    watch('src/image/*', imageMinify);
+    watch('src/js/*', concatJs);
+}
+
+// // Browser Sync
+// function browserSync() {
+//     browserSync.init({
+//         server: 'dist/',
+//         port: 8080
+//     })
+// }
+
+exports.watch = parallel(watchFiles);
+exports.default = series(clear, parallel(compileScss, copyHtml, imageMinify, concatJs));
+
+// exports.build = series(clean, parallel(compileScss, copyHtml, imageMinify, concatJs));
 
 
-
-exports.compileScss = compileScss;
-exports.copyHtml = copyHtml;
+// exports.compileScss = compileScss;
+// exports.copyHtml = copyHtml;
+// exports.imageMinify = imageMinify;
+// exports.concatJs = concatJs;
